396d11fc3a387caefc5c281b942f8b19e7c88c4d
SamuelCandido <samueljosecandido9@gmail.com>
2022-10-13 08:31:38 -0300

    Teste de album operante :)

--- back/tests/test_album.py
+++ back/tests/test_album.py
@@ -6,13 +6,13 @@ from modelos.album import Album
 
 class TestAlbum(unittest.TestCase):
     def test_coin(self):
-        a1 = Album(nome="TesteDeAlbum", ano="1999")
+        a1 = Album(nome="TesteDeAlbum", descricao="1999")
         db.session.add(a1)
         db.session.commit()
 
         a1 = Album.query.filter_by(nome="TesteDeAlbum").first()
         self.assertEqual(a1.nome, "TesteDeAlbum")
-        self.assertEqual(a1.ano, "1999")
+        self.assertEqual(a1.descricao, "1999")
 
         db.session.delete(a1)
         db.session.commit()

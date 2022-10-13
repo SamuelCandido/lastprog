ddc7cfb5189d6d1caa65699164e17d027e019518
YaraRahn <yararahn@gmail.com>
2022-10-13 08:46:48 -0300

    Informamos o ip

--- back/servidor-back.py
+++ back/servidor-back.py
@@ -7,7 +7,7 @@ from rotas.login import *
 
 @app.route("/")
 def inicio():
-    return 'Sistema de cadastro de moedas. '+\
+    return 'Este é o nosso sistema de cadastro de moedas, que está disponível no ip 191.52.7.80. Todas as rotas e os testes com curl estão disponíveis no arquivo "servidor-back.py".'+\
         '<a href="/listar_moedas">Operação listar</a>'
 
 @app.route("/listar_moedas")

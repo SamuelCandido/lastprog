df9e7773d63e8df8684988f8dc9204afcefefa69
SamuelCandido <samueljosecandido9@gmail.com>
2022-10-17 11:01:26 -0300

    Pequenas mudanças

--- back/rotas/cadastro.py
+++ back/rotas/cadastro.py
@@ -9,10 +9,22 @@ def cadastro():
     dados = request.get.json(force=True)
     if dados["nome"] == "" or dados["email"] == "" or dados["senha"] == "": 
         return jsonify({"resultado":"erro", "detalhes":"Nome de usuario não pode ser vazio"})
+    nome = dados['nome']
     email = dados['email']
     senha = dados['senha']
 
-    encontrado = Usuario.query.filter_by(email=email, senha=cifrar(senha)).first()
+    usuario = Usuario.query.filter_by(nome=nome, email=email, senha=cifrar(senha)).first()
     
     if encontrado != None: 
         resposta = jsonify({"resultado": "erro", "detalhes":" Usuario já existente"})
+
+    try: # tentar executar a operação
+      nova = Usuario(email=email, senha=cifrar(senha))# criar a nova pessoa
+      db.session.add(nova) # adicionar no BD
+      db.session.commit() # efetivar a operação de gravação
+    except Exception as e: # em caso de erro...
+      # informar mensagem de erro
+      resposta = jsonify({"resultado":"erro", "detalhes":str(e)})
+    # adicionar cabeçalho de liberação de origem
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta # responder!
